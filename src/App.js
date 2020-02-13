d365f14b5109a6823407b42d1ef0191577d0cafd
nsns0101 <nsns0101@naver.com>
2020-02-13 14:43:03 +0900

    component_functions

--- src/App.js
+++ src/App.js
@@ -2,28 +2,20 @@ import React from 'react';
 
 //class Component  (function Component를 쓰면 동적 state를 못씀)
 class App extends React.Component {
+
+  //생성자 함수
+  constructor(props) {
+    super(props);
+    console.log("constructor")
+  }
+
   //동적으로 바꿀 값을 넣을 것
   state = {
     count: 0,
 
   }
-  render() {  //React는 Class의 render메서드를 자동실행
-    return (
-      <div>
-        <h1> The number is : {this.state.count}</h1>
-        <button onClick={this._add}>Add</button>
-        <button onClick={this._minus}>Minus</button>
-      </div>
-    );
-  };
 
   _add = () => {
-    //setState를 쓰지않고 this.state.count = ~~ 이런 방식으로 할시 
-    //class component의 render메서드를 호출하지 않으므로 setState()를 사용할 것
-
-    // this.setState({
-    //   count: this.state.count + 1 // ++연산자는 사용안함
-    // });
     this.setState(current => ({
       count: current.count + 1
     }));
@@ -34,6 +26,26 @@ class App extends React.Component {
     }));
   };
 
+  //웹이 실행될 때 실행되는 함수
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  //웹이 업데이트 될 때 실행
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+
+  render() {  //React는 Class의 render메서드를 자동실행
+    return (
+      <div>
+        <h1> The number is : {this.state.count}</h1>
+        <button onClick={this._add}>Add</button>
+        <button onClick={this._minus}>Minus</button>
+      </div>
+    );
+  };
 }
 
 
