98156b72e774103818782d3570ff07b366d43509
nsns0101 <nsns0101@naver.com>
2020-02-14 21:38:51 +0900

    css, git_page

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # > React를 이용한 Movie Web <
 
-# 필수조건
-- node.js 설치
-- npx 설치 : npm install npx -g
+# Start 조건
+- 1. node.js install
+- 2. npx install
+    npm install npx -g
 
-
-# npm install
+# Fetch Data from an API 
 - npm i axios     (data fetch (API) )
+
+# API used
+- Move API 
+    https://yts-proxy.now.sh/list_movies.json?sort_by=rating
+
+# GitHub Page
+- 1. npm i gh-pages     : 깃 허브 업로드 권한을 허가해주는 모듈
+- 2. package.json
+format) "homepage" : "https://{Your GitHub ID}.github.io/{Repository Name}"
+    "homepage" : "https://nsns0101.github.io/React_Project_Movie"
+추가
+- 3. package.json
+scripts안에
+    "deploy": "gh-pages -d build",
+    "predeploy": "npm run build" 
+추가
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';    //fetch로 get하는 것 보다 시간 절약
-
+import Movie from './Movie.js';
+import "./css/App.css"
 //class Component  (function Component를 쓰면 동적 state를 못씀)
 class App extends React.Component {
 
@@ -11,9 +12,19 @@ class App extends React.Component {
   }
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log("a" + movies);
-    this.state.isLoading = false;
+    const {
+      data: {
+        data: { movies }
+      } //movies.data.data.movies
+      //sort_by=rating은 순위대로 정렬
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // console.log(movies);
+
+    this.setState({
+      movies,    //movies : movies
+      isLoading: false,
+    });
+
   }
 
   //render 함수가 실행되면 호출하는 함수
@@ -24,14 +35,38 @@ class App extends React.Component {
 
   render() {  //React는 Class의 render메서드를 자동실행
     // console.log("render 함수 실행");     
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading ? "Loading..." : "We are ready"}
-      </div>
+      <section className="container">
+        {
+          isLoading ? (
+            <div className="loader">
+              <span className="loader__text">Loading...</span>
+            </div>
+          ) : (
+              <div className="movies">
+                {
+                  movies.map(movie => (
+                    // console.log(movie);
+                    <Movie
+                      key={movie.id}
+                      id={movie.id}
+                      year={movie.year}
+                      genres={movie.genres}
+                      title={movie.title}
+                      summary={movie.summary}
+                      poster={movie.medium_cover_image}
+                    />
+                  ))
+                }
+              </div>
+            )
+        }
+      </section>
     );
 
-  };
+
+  }
 }
 
 
